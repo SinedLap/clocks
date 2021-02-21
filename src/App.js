9b4acfcc876e9dfa5e06b2f567378541373b100e
SinedLap <57713406+SinedLap@users.noreply.github.com>
2021-02-21 17:22:26 +0200

    Added basic layout. Deleted react logo.svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,6 +6,12 @@ function App() {
       <header className="app-header">
         <h1>UTC CLOCKS</h1>
       </header>
+      <main>
+        <h1>Clocks AREA</h1>
+      </main>
+      <footer>
+        <p>Sponsored by new Date().com</p>
+      </footer>
     </div>
   );
 }
