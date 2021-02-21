d62c4546c5a1e107bf59ed083f7bdd5c524b7859
SinedLap <57713406+SinedLap@users.noreply.github.com>
2021-02-21 17:04:04 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h1>UTC CLOCKS</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
